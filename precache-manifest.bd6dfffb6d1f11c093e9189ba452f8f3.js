246d05093131629f6734e317d6d78afa69ed50d9
monikasunle <2680655949@qq.com>
2020-10-25 17:20:24 +0800

    taobao

---
+++ precache-manifest.bd6dfffb6d1f11c093e9189ba452f8f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8d465cbf37d717784cbe0e64f9e9cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d9cd1d472ed16e75671",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "481634d3773fe5441254",
+    "url": "/static/css/main.ad64c229.chunk.css"
+  },
+  {
+    "revision": "9d9cd1d472ed16e75671",
+    "url": "/static/js/2.cbf013a5.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.cbf013a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481634d3773fe5441254",
+    "url": "/static/js/main.7123f8c4.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
